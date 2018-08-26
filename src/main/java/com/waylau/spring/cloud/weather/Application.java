b7c2084f3396a4375bce1829d470247c0d4ffc1c
zhangyibo1012 <35024391+zhangyibo1012@users.noreply.github.com>
2018-08-26 13:52:28 +0800

    小行星
    信息

--- src/main/java/com/waylau/spring/cloud/weather/Application.java
+++ src/main/java/com/waylau/spring/cloud/weather/Application.java
@@ -2,6 +2,7 @@ package com.waylau.spring.cloud.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +10,6 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+
 }
